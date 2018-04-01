e6469959963bf25d9f13fc673b8eef21bd1d13bd
olbesp <olbesp365@gmail.com>
2018-04-01 15:34:15 +0300

    Get correct response from API

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 
-// import styles from './App.scss';
+import styles from './App.scss';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    userInput: '',
+    searchResults: null
+  }
+
+  inputChangedHandler = (e) => {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+
+  searchSentHandler = () => {
+    const request = encodeURIComponent(this.state.userInput);
+    axios.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${request}&limit=10&namespace=0&origin=*&format=json`)
+      .then(response => {
+        console.log(response);
+      })
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.state.userInput !== nextState.userInput;
+  }
+
+
   render() {
     return (
-      <div>
-
+      <div className={styles.App}>
+        <div>
+          <input type="text" onChange={this.inputChangedHandler} />
+          <button onClick={this.searchSentHandler}>Search</button>
+        </div>
       </div>
     );
   }
