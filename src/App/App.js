67e60f783227d062f2375beb174c7e3dc79d27eb
olbesp <olbesp365@gmail.com>
2018-04-01 15:47:50 +0300

    Add input keypress handler

--- src/App/App.js
+++ src/App/App.js
@@ -9,18 +9,28 @@ class App extends Component {
     searchResults: null
   }
 
+  getData = () => {
+    const request = encodeURIComponent(this.state.userInput);
+    axios.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${request}&limit=10&namespace=0&origin=*&format=json`)
+      .then(response => {
+        console.log(response);
+      })
+  }
+
   inputChangedHandler = (e) => {
     this.setState({
       userInput: e.target.value
     });
   }
 
-  searchSentHandler = () => {
-    const request = encodeURIComponent(this.state.userInput);
-    axios.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${request}&limit=10&namespace=0&origin=*&format=json`)
-      .then(response => {
-        console.log(response);
-      })
+  searchClickedHandler = () => {
+    this.getData();
+  }
+
+  inputPressedHandler = (e) => {
+    if ((e.which === 13 || e.charCode === 13) && this.state.userInput.trim()) {
+      this.getData();
+    }
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -32,8 +42,8 @@ class App extends Component {
     return (
       <div className={styles.App}>
         <div>
-          <input type="text" onChange={this.inputChangedHandler} />
-          <button onClick={this.searchSentHandler}>Search</button>
+          <input type="text" onChange={this.inputChangedHandler} onKeyPress={this.inputPressedHandler} />
+          <button onClick={this.searchClickedHandler}>Search</button>
         </div>
       </div>
     );
